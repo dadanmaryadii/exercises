a06625d9d2c1ba6c08cb5920510db115c090d00b
dadanmaryadii <40693509+dadanmaryadii@users.noreply.github.com>
2018-08-03 18:48:30 +0700

    area of Square.js

---
+++ areaOfSquare.js
@@ -0,0 +1,11 @@
+function square (number) {
+    return number*number;
+  }
+  
+  function pi () {
+    return 3,14;  // Your code here
+  }
+  
+  
+  var area = math.round(pi() * square(7));
+  console.log(area); // 154
